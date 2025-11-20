94db48c59ad1fde49ebdb1d891e8d3dabed659ff
LentilDaddy <Josephine_ILStone@proton.me>
2025-11-20 16:10:08 +0000

    add accessible colours and split cost and useful photons graohs into two

--- build/compare.C
+++ build/compare.C
@@ -36,10 +36,25 @@ struct HistogramDecoration {
 
 
 struct EnergyResult {
-    std::string energy;
+    std::string energyLabel;
+    double energy;
     double foilThickness;
     double usefulPhotons;
 };
+
+
+int kP10Cyan = TColor::GetColor("#92dadd");
+int kP10Ash = TColor::GetColor("#717581");
+int kP10Green = TColor::GetColor("#b9ac70");
+int kP10Orange = TColor::GetColor("#e76300");
+int kP10Brown = TColor::Get("#a96b59");
+int kP10Violet = TColor::GetColor("#832db6");
+int kP10Gray = TColor::GetColor("#94a4a2");
+int kP10Red = TColor::GetColor("#bd1f01");
+int kP10Yellow = TColor::GetColor("#ffa90e");
+int kP10Blue = TColor::GetColor("#3f90da");
+
+
 
 
 void compare()
@@ -50,12 +65,12 @@ void compare()
     std::vector<std::pair<std::string, TChain*>> chains;
 
     // std::vector<std::string> energies = {"20MeV", "22MeV", "25MeV", "28MeV", "30MeV", "35MeV", "40MeV", "45MeV", "50MeV"};
-    std::vector<std::string> energies = {"20MeV", "25MeV", "30MeV", "35MeV", "40MeV", "45MeV", "50MeV"};
+    // std::vector<std::string> energies = {"20MeV", "25MeV", "30MeV", "35MeV", "40MeV", "45MeV", "50MeV"};
     // std::vector<std::string> energies = {"25MeV", "50MeV"};
-    // std::vector<std::string> energies = {"35MeV"};
+    std::vector<std::string> energies = {"40MeV"};
 
-    std::vector<std::string> foilThicknesses = {"1mm","2mm","3mm","4mm","5mm","6mm","7mm","8mm","9mm","10mm", "11mm", "12mm", "13mm", "14mm", "15mm", "16mm", "17mm", "18mm", "19mm", "20mm", "21mm", "22mm"};
-    // std::vector<std::string> foilThicknesses = {"10mm", "11mm", "12mm", "13mm", "14mm", "15mm", "16mm", "17mm", "18mm", "20mm", "22mm"};
+    // std::vector<std::string> foilThicknesses = {"1mm","2mm","3mm","4mm","5mm","6mm","7mm","8mm","9mm","10mm", "11mm", "12mm", "13mm", "14mm", "15mm", "16mm", "17mm", "18mm", "19mm", "20mm", "21mm", "22mm"};
+    std::vector<std::string> foilThicknesses = {"10mm", "11mm"}; //test
 
     std::vector<EnergyResult> results;
 
@@ -74,7 +89,7 @@ void compare()
     // Decorations and setup
     //===============================
     HistogramDecoration Decoration = {
-       1, kBlack, "Depth (cm)", "#photons in range 15-22 MeV", nullptr, 0., 100., 0., 0.
+       1, kP10Brown, "Depth (cm)", "#photons in range 15-22 MeV", nullptr, 0., 100., 0., 0.
     };
 
     int colors[] = {kP10Red, kP10Cyan, kP10Ash, kP10Green, kP10Orange, kP10Brown, kP10Violet, kP10Gray,
@@ -94,13 +109,21 @@ for (size_t i = 0; i < chains.size(); i++) {
 
     // Extract foil thickness
     double foilThickness = 0.0;
-    auto pos = label.find("mm");
-    if (pos != std::string::npos) {
-        size_t start = label.find_last_of('_', pos); //find underscore before "mm"
-        foilThickness = std::stod(label.substr(start + 1, pos - start - 1));
+    auto pos_MeV = label.find("mm");
+    if (pos_MeV != std::string::npos) {
+        size_t start = label.find_last_of('_', pos_MeV); //find underscore before "mm"
+        foilThickness = std::stod(label.substr(start + 1, pos_MeV - start - 1));
     } else continue;
 
-    std::string energy = label.substr(label.find("_") + 1);
+    std::string energyLabel = label.substr(label.find("_") + 1);
+
+    // Extract foil thickness
+    double energy = 0.0;
+    auto pos = label.find("MeV");
+    if (pos != std::string::npos) {
+        size_t start = label.find_last_of('_', pos); 
+        energy = std::stod(label.substr(start + 1, pos - start - 1));
+    } else continue;
 
     if (!t || t->GetEntries() == 0) {
         std::cout << "Warning: Chain " << label << " is empty!" << std::endl;
@@ -145,9 +168,8 @@ for (size_t i = 0; i < chains.size(); i++) {
     histos.push_back(h);
     legend->AddEntry(h, label.c_str(), "l");
 
-    results.push_back({energy, foilThickness, integral / 1e6});
+    results.push_back({energyLabel, energy, foilThickness, integral / 1e6});
 }
-
 
     //===============================
     // Scatter plot of useful photons vs beam energy
@@ -162,24 +184,26 @@ for (size_t i = 0; i < chains.size(); i++) {
     TGraph *g45MeV = new TGraph();
     TGraph *g50MeV = new TGraph();
 
+
+    //this needs to be converted to double for the eneryg!!!!!
     for (const auto& r : results) {
-        if (r.energy == "20MeV")      g20MeV->SetPoint(g20MeV->GetN(), r.foilThickness, r.usefulPhotons);
-        else if (r.energy == "22MeV") g22MeV->SetPoint(g22MeV->GetN(), r.foilThickness, r.usefulPhotons);
-        else if (r.energy == "25MeV") g25MeV->SetPoint(g25MeV->GetN(), r.foilThickness, r.usefulPhotons);
-        else if (r.energy == "28MeV") g28MeV->SetPoint(g28MeV->GetN(), r.foilThickness, r.usefulPhotons);
-        else if (r.energy == "30MeV") g30MeV->SetPoint(g30MeV->GetN(), r.foilThickness, r.usefulPhotons);
-        else if (r.energy == "35MeV") g35MeV->SetPoint(g35MeV->GetN(), r.foilThickness, r.usefulPhotons);
-        else if (r.energy == "40MeV") g40MeV->SetPoint(g40MeV->GetN(), r.foilThickness, r.usefulPhotons);
-        else if (r.energy == "45MeV") g45MeV->SetPoint(g45MeV->GetN(), r.foilThickness, r.usefulPhotons);
-        else if (r.energy == "50MeV") g50MeV->SetPoint(g50MeV->GetN(), r.foilThickness, r.usefulPhotons);
+        if (r.energyLabel == "20MeV")      g20MeV->SetPoint(g20MeV->GetN(), r.foilThickness, r.usefulPhotons);
+        else if (r.energyLabel == "22MeV") g22MeV->SetPoint(g22MeV->GetN(), r.foilThickness, r.usefulPhotons);
+        else if (r.energyLabel == "25MeV") g25MeV->SetPoint(g25MeV->GetN(), r.foilThickness, r.usefulPhotons);
+        else if (r.energyLabel == "28MeV") g28MeV->SetPoint(g28MeV->GetN(), r.foilThickness, r.usefulPhotons);
+        else if (r.energyLabel == "30MeV") g30MeV->SetPoint(g30MeV->GetN(), r.foilThickness, r.usefulPhotons);
+        else if (r.energyLabel == "35MeV") g35MeV->SetPoint(g35MeV->GetN(), r.foilThickness, r.usefulPhotons);
+        else if (r.energyLabel == "40MeV") g40MeV->SetPoint(g40MeV->GetN(), r.foilThickness, r.usefulPhotons);
+        else if (r.energyLabel == "45MeV") g45MeV->SetPoint(g45MeV->GetN(), r.foilThickness, r.usefulPhotons);
+        else if (r.energyLabel == "50MeV") g50MeV->SetPoint(g50MeV->GetN(), r.foilThickness, r.usefulPhotons);
     }
-    //need to change this to have differenet energies now instead of different media.
+    //change this to accessible colour scheme ROOT
 
     // Style graphs
     g20MeV->SetMarkerColor(kP10Red);   g20MeV->SetMarkerStyle(21);
-    // g22MeV->SetMarkerColor(kCyan);   g22MeV->SetMarkerStyle(22);
+    // g22MeV->SetMarkerColor(kP10Cyan);   g22MeV->SetMarkerStyle(22);
     g25MeV->SetMarkerColor(kP10Cyan);  g25MeV->SetMarkerStyle(23);
-    // g28MeV->SetMarkerColor(kYellow); g28MeV->SetMarkerStyle(24);
+    // g28MeV->SetMarkerColor(kP10Yellow); g28MeV->SetMarkerStyle(24);
     g30MeV->SetMarkerColor(kP10Ash); g30MeV->SetMarkerStyle(25);
     g35MeV->SetMarkerColor(kP10Green);    g35MeV->SetMarkerStyle(26);
     g40MeV->SetMarkerColor(kP10Orange);g40MeV->SetMarkerStyle(27);
@@ -197,7 +221,6 @@ for (size_t i = 0; i < chains.size(); i++) {
         }
     }
 
-    //rest should be serial
     TMultiGraph *mg = new TMultiGraph();
     mg->Add(g20MeV, "P");
     mg->Add(g22MeV, "P");
@@ -211,10 +234,6 @@ for (size_t i = 0; i < chains.size(); i++) {
 
     mg->SetMaximum(YMax * 1.1);
 
-
-    //legend on the top right:
-    // TLegend *scatterLegend = new TLegend(0.7, 0.7, 0.9, 0.9);
-    //legend on top left:
     TLegend *scatterLegend = new TLegend(0.15, 0.7, 0.35, 0.9);
     scatterLegend->AddEntry(g20MeV, "20MeV", "p");
     // scatterLegend->AddEntry(g22MeV, "22MeV", "p");
@@ -227,18 +246,34 @@ for (size_t i = 0; i < chains.size(); i++) {
     scatterLegend->AddEntry(g50MeV, "50MeV", "p");
 
     TCanvas *c3 = new TCanvas("c3", "#Useful Photons vs Foil Thickness", 600, 500);
-    c3->SetRightMargin(0.15);
+    // c3->SetRightMargin(0.15);
     c3->SetLeftMargin(0.15);
-    mg->SetTitle("Total #Useful photons (15-22MeV) in Detector per Primary Electron vs Foil Thickness;Foil Thickness (mm);#Useful photons per Primary Electron");
+
+
+    mg->SetTitle("Total #Useful photons (15-22MeV) in Detector per Primary Electron vs Foil Thickness");
+    mg->GetXaxis()->SetTitle("Foil Thickness (mm)");
+    mg->GetYaxis()->SetTitle("#Useful photons per Primary Electron");
+    mg->GetXaxis()->SetTitleSize(0.05);
+    mg->GetYaxis()->SetTitleSize(0.05);
+    mg->GetXaxis()->SetLabelSize(0.04);
+    mg->GetYaxis()->SetLabelSize(0.04);
     mg->Draw("AP");
     mg->GetXaxis()->SetLimits(0, 24);
 
 
+    // c3->Update();
+    c3->cd();
+    scatterLegend->Draw();
+    c3->Update();
+    c3->SaveAs("Photons_FoilThickness_latest.png");
 
-/*Create additional scatter graphs in corresponding colours matching a right hand axis. 
-Relationship should be cost per useful photon = (beam energy)^2 / (num photons per primary electron). 
-Also need to normalise it and make dynamic y axis range...*/
 
+
+    /*Cost Graph different canvas AND save as different file*/
+
+
+
+    TMultiGraph *mg_Price = new TMultiGraph();
 
     TGraph *g20MeV_Price = new TGraph();
     TGraph *g22MeV_Price = new TGraph();
@@ -253,43 +288,42 @@ Also need to normalise it and make dynamic y axis range...*/
 
 
     for (const auto& r : results) {
-        if (r.energy == "20MeV")      g20MeV_Price->SetPoint(g20MeV->GetN(), r.foilThickness, (r.energy)^2/(r.usefulPhotons));
-        else if (r.energy == "22MeV") g22MeV_Price->SetPoint(g22MeV->GetN(), r.foilThickness, (r.energy)^2/(r.usefulPhotons));
-        else if (r.energy == "25MeV") g25MeV_Price->SetPoint(g25MeV->GetN(), r.foilThickness, (r.energy)^2/(r.usefulPhotons));
-        else if (r.energy == "28MeV") g28MeV_Price->SetPoint(g28MeV->GetN(), r.foilThickness, (r.energy)^2/(r.usefulPhotons));
-        else if (r.energy == "30MeV") g30MeV_Price->SetPoint(g30MeV->GetN(), r.foilThickness, (r.energy)^2/(r.usefulPhotons));
-        else if (r.energy == "35MeV") g35MeV_Price->SetPoint(g35MeV->GetN(), r.foilThickness, (r.energy)^2/(r.usefulPhotons));
-        else if (r.energy == "40MeV") g40MeV_Price->SetPoint(g40MeV->GetN(), r.foilThickness, (r.energy)^2/(r.usefulPhotons));
-        else if (r.energy == "45MeV") g45MeV_Price->SetPoint(g45MeV->GetN(), r.foilThickness, (r.energy)^2/(r.usefulPhotons));
-        else if (r.energy == "50MeV") g50MeV_Price->SetPoint(g50MeV->GetN(), r.foilThickness, (r.energy)^2/(r.usefulPhotons));
+        double denom = (r.usefulPhotons > 0.0) ? r.usefulPhotons : 1e-12;
+        double cost = (r.energy * r.energy) / denom;
+        if (r.energyLabel == "20MeV")      g20MeV_Price->SetPoint(g20MeV_Price->GetN(), r.foilThickness, cost);
+        else if (r.energyLabel == "22MeV") g22MeV_Price->SetPoint(g22MeV_Price->GetN(), r.foilThickness, cost);
+        else if (r.energyLabel == "25MeV") g25MeV_Price->SetPoint(g25MeV_Price->GetN(), r.foilThickness, cost);
+        else if (r.energyLabel == "28MeV") g28MeV_Price->SetPoint(g28MeV_Price->GetN(), r.foilThickness, cost);
+        else if (r.energyLabel == "30MeV") g30MeV_Price->SetPoint(g30MeV_Price->GetN(), r.foilThickness, cost);
+        else if (r.energyLabel == "35MeV") g35MeV_Price->SetPoint(g35MeV_Price->GetN(), r.foilThickness, cost);
+        else if (r.energyLabel == "40MeV") g40MeV_Price->SetPoint(g40MeV_Price->GetN(), r.foilThickness, cost);
+        else if (r.energyLabel == "45MeV") g45MeV_Price->SetPoint(g45MeV_Price->GetN(), r.foilThickness, cost);
+        else if (r.energyLabel == "50MeV") g50MeV_Price->SetPoint(g50MeV_Price->GetN(), r.foilThickness, cost);
     }
 
 
-
     double YRightMax = -1e9;
-    double RRightMin = 1e9;
+    double YRightMin = 1e9;
     for (auto g : {g20MeV_Price, g22MeV_Price, g25MeV_Price, g28MeV_Price, g30MeV_Price, g35MeV_Price, g40MeV_Price, g45MeV_Price, g50MeV_Price}) {
         int n = g->GetN(); //number of points in each graph
         for (int i = 0; i < n; ++i) {
             double x, y;
             g->GetPoint(i, x, y);
             if (y > YRightMax) YRightMax = y;
-            if (y < RRightMin) RRightMin = y;
+            if (y < YRightMin) YRightMin = y;
         }
     }
 
 
     // Style graphs
-    g20MeV_Price->SetMarkerColor(kP10Red);   g20MeV->SetMarkerStyle(8);
-    g25MeV_Price->SetMarkerColor(kP10Cyan);  g25MeV->SetMarkerStyle(8);
-    g30MeV_Price->SetMarkerColor(kP10Ash); g30MeV->SetMarkerStyle(8);
-    g35MeV_Price->SetMarkerColor(kP10Green);    g35MeV->SetMarkerStyle(8);
-    g40MeV_Price->SetMarkerColor(kP10Orange);g40MeV->SetMarkerStyle(8);
-    g45MeV_Price->SetMarkerColor(kP10Brown);  g45MeV->SetMarkerStyle(8);
-    g50MeV_Price->SetMarkerColor(kP10Gray);   g50MeV->SetMarkerStyle(8);
+    g20MeV_Price->SetMarkerColor(kP10Red);   g20MeV_Price->SetMarkerStyle(8);
+    g25MeV_Price->SetMarkerColor(kP10Cyan);  g25MeV_Price->SetMarkerStyle(8);
+    g30MeV_Price->SetMarkerColor(kP10Ash); g30MeV_Price->SetMarkerStyle(8);
+    g35MeV_Price->SetMarkerColor(kP10Green);    g35MeV_Price->SetMarkerStyle(8);
+    g40MeV_Price->SetMarkerColor(kP10Orange);g40MeV_Price->SetMarkerStyle(8);
+    g45MeV_Price->SetMarkerColor(kP10Brown);  g45MeV_Price->SetMarkerStyle(8);
+    g50MeV_Price->SetMarkerColor(kP10Gray);   g50MeV_Price->SetMarkerStyle(8);
 
-        // Create a second multigraph for the price data
-    TMultiGraph *mg_Price = new TMultiGraph();
     mg_Price->Add(g20MeV_Price, "P");
     mg_Price->Add(g25MeV_Price, "P");
     mg_Price->Add(g30MeV_Price, "P");
@@ -298,27 +332,42 @@ Also need to normalise it and make dynamic y axis range...*/
     mg_Price->Add(g45MeV_Price, "P");
     mg_Price->Add(g50MeV_Price, "P");
 
-    // Draw price graphs with right axis scaling
-    mg_Price->Draw("P same");
+    mg_Price->SetMaximum(YRightMax * 1.1);
 
 
-    double yPadMin = gPad->GetUymin();
-    double yPadMax = gPad->GetUymax();
-    double xPadMax = 24; // right x position for the TGaxis
+    TLegend *scatterLegend_Price = new TLegend(0.15, 0.7, 0.35, 0.9);
+    scatterLegend_Price->AddEntry(g20MeV_Price, "20MeV", "p");
+    // scatterLegend_Price->AddEntry(g22MeV, "22MeV", "p");
+    scatterLegend_Price->AddEntry(g25MeV_Price, "25MeV", "p");
+    // scatterLegend_Price->AddEntry(g28MeV, "28MeV", "p");
+    scatterLegend_Price->AddEntry(g30MeV_Price, "30MeV", "p");
+    scatterLegend_Price->AddEntry(g35MeV_Price, "35MeV", "p");
+    scatterLegend_Price->AddEntry(g40MeV_Price, "40MeV", "p");
+    scatterLegend_Price->AddEntry(g45MeV_Price, "45MeV", "p");
+    scatterLegend_Price->AddEntry(g50MeV_Price, "50MeV", "p");
+
+    TCanvas *c4 = new TCanvas("c4", "Relative Cost per Useful Photon", 600, 500);
+    // c3->SetRightMargin(0.15);
+    c4->SetLeftMargin(0.15);
 
 
-    // // Fixed right-axis range requested by you:
-    const double costAxisMin = YRightMin;
-    const double costAxisMax = YRightMax;
+    mg_Price->SetTitle("Relative Cost per Useful Photon vs Foil Thickness");
+    mg_Price->GetXaxis()->SetTitle("Foil Thickness (mm)");
+    mg_Price->GetYaxis()->SetTitle("Cost per Useful Photon");
+    mg_Price->GetXaxis()->SetTitleSize(0.05);
+    mg_Price->GetYaxis()->SetTitleSize(0.05);
+    mg_Price->GetXaxis()->SetLabelSize(0.04);
+    mg_Price->GetYaxis()->SetLabelSize(0.04);
+    mg_Price->Draw("AP");
+    mg_Price->GetXaxis()->SetLimits(0, 24);
 
-    TGaxis *rightAxis = new TGaxis(xPadMax, yPadMin, xPadMax, yPadMax, costAxisMin, costAxisMax, 510, "+L");
-    rightAxis->SetTitle("Relative Cost per Useful Photon");
-    rightAxis->SetTitleOffset(1.2);
-    rightAxis->Draw();
 
-    scatterLegend->Draw();
-    c3->Update();
-    c3->SaveAs("Photons_FoilThickness_latest.png");
+    // c3->Update();
+    c4->cd();
+    scatterLegend_Price->Draw();
+    c4->Update();
+    c4->SaveAs("CostPerUsefulPhoton.png");
+
 }
 
 int main() {
