f57037c82591c94d7ab29ca98fb45ee16110c87d
LentilDaddy <Josephine_ILStone@proton.me>
2025-12-15 18:16:18 +0000

    add full set of energies

--- build/compare.C
+++ build/compare.C
@@ -61,8 +61,8 @@ void compare()
 
     // std::vector<std::string> mediums = {"SF6", "C3F8", "CF4", "PF5", "UF6", "Vacuum"};
     std::vector<std::string> mediums = {"SF6"};
-    // std::vector<std::string> energies = {"20MeV", "22MeV", "25MeV", "28MeV", "30MeV", "35MeV", "40MeV", "45MeV", "50MeV"};
-    std::vector<std::string> energies = {"30MeV"};
+    std::vector<std::string> energies = {"20MeV", "25MeV", "30MeV", "35MeV", "40MeV", "45MeV", "50MeV"};
+    // std::vector<std::string> energies = {"30MeV"};
 
     // create one TChain per (medium, energy) and add matching files immediately
     for (const auto &m : mediums) {
